1e3a401a4c7da5cb7b0b62e59d6006a80204bbcc
Даниил <daniil.grra@gmail.com>
2026-08-23 15:19:51 +0300

    Update env.py

--- src/models/migration/env.py
+++ src/models/migration/env.py
@@ -1,9 +1,3 @@
-import sys
-import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, '../../../'))
-sys.path.insert(0, project_root)
-
 import asyncio
 from logging.config import fileConfig
 
